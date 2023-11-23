b47e89f7b15749d32f59899ae8512010d916d71a
Admini strator <macoton@104.net>
2023-11-24 08:07:02 +0900

    20231124080657

---
+++ README.md
@@ -0,0 +1,29 @@
+# a
+
+Tinker Board 2s Kernel<br>
+[→](https://tinker-board.asus.com/jp/download-list.html?product=tinker-board-2s)<br>
+[→](https://github.com/TinkerBoard2/kernel/branches)<br>
+
+# b
+
+Tinker Board 2s github<br>
+[→](https://tinker-board.asus.com/jp/download-list.html?product=tinker-board-2s)<br>
+d<br>
+e<br>
+
+# c
+
+C:\Program Files\qemu\
+
+& 'C:\Program Files\qemu\qemu-system-x86_64.exe'
+ -m 4096 -smp 2 -hda MSEdge\ -\ Win10.vhdx 
+
+## 1a
+
+### 2a
+
+#### 3a
+
+##### 4a
+
+###### 5a
---
+++ test/作業用.ps1
@@ -0,0 +1,24 @@
+
+& dir 'C:\Program Files\qemu'
+
+& 'C:\Program Files\qemu\qemu-system-x86_64.exe' --h | Set-Clipboard
+
+& 'C:\Program Files\qemu\qemu-system-x86_64.exe' -m 4G -hda D:\Ubuntu.vdi
+
+& 'C:\Users\macot\AppData\Local\Android\Sdk\emulator\qemu\windows-x86_64\qemu-system-x86_64.exe' D:\Ubuntu.vdi
+
+& 'C:\Program Files\qemu\qemu-system-aarch64.exe' --h | Set-Clipboard
+
+& 'C:\Program Files\qemu\qemu-system-aarch64.exe' -machine virt -m 4G -hda C:\git\linuxCLabo\Tinker_Board_2-Debian-Bullseye-v3.0.6-20230627\Tinker_Board_2-Debian-Bullseye-v3.0.6-20230627.img
+
+& 'C:\Program Files\qemu\qemu-system-aarch64.exe' -machine help -m 4G -hda C:\git\linuxCLabo\Tinker_Board_2-Debian-Bullseye-v3.0.6-20230627\Tinker_Board_2-Debian-Bullseye-v3.0.6-20230627.img
+
+& 'C:\Program Files\qemu\qemu-system-aarch64.exe' -machine help -m 4G -hda C:\git\linuxCLabo\Tinker_Board_2-Debian-Bullseye-v3.0.6-20230627\Tinker_Board_2-Debian-Bullseye-v3.0.6-20230627.img
+
+https://tinker-board.asus.com/jp/download-list.html?product=tinker-board-2s
+https://qemu.weilnetz.de/w64/
+https://github.com/TinkerBoard/debian_kernel/releases
+https://github.com/TinkerBoard2
+https://jp.minitool.com/partition-disk/qemu-for-windows.html
+
+
